8b59df12e57beeb42902c487c297a38ce3580032
Numan <elahinouman.elahi@gmail.com>
2016-04-29 18:01:35 +0530

    Finally done

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var TelegramBot = require('node-telegram-bot-api');
+var unirest = require('unirest');
 
 var token = '186253056:AAH_1mmK5DFdSe-4JwsvM2w8ydtHoD-1dro';
+var key = 'trnsl.1.1.20160429T055226Z.ad9ff8a646a39d38.2791ff1f75fc277eafc53bd34e7205c35d4c7cf4';
 // See https://developers.openshift.com/en/node-js-environment-variables.html
 var port = process.env.OPENSHIFT_NODEJS_PORT;
 var host = process.env.OPENSHIFT_NODEJS_IP;
@@ -11,5 +13,16 @@ var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
 bot.setWebHook(domain+':443/bot'+token);
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Asalamualaikum wa rehmatullah");
+  var user = msg.from.username;
+  if(msg.text)
+  {
+	  unirest.post('https://translate.yandex.net/api/v1.5/tr.json/translate')
+        .send({ "key": key, "text": msg.text, "lang":"en", "options":1 })
+        .end(function (response) {
+            var resp = JSON.parse(response.raw_body);
+			if(resp.detected.lang !== "en"){
+				bot.sendMessage(chatId, user + " says : "+resp.text[0]);
+			}			
+        });	  
+  }
 });
